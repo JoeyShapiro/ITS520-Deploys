b78c8433474576fe4eb6faf8d30bfe40edb63520
JoeyShapiro <joeyashapiro@gmail.com>
2023-10-11 19:00:17 -0500

    new test

--- WineQuality/XGBoost/index.js
+++ WineQuality/XGBoost/index.js
@@ -1,6 +1,6 @@
 
 async function runExample() {
-  var x = new Float32Array(11);
+  var x = new Float32Array(1);
   x[0] = document.getElementById('box1').value;
   x[1] = document.getElementById('box2').value;
   x[2] = document.getElementById('box3').value;
